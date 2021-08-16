9a6c67cd589a97935df0d464376381424c17a8bd
Konsta Purtsi <konsta@identio.fi>
2021-08-16 18:45:30 +0300

    Fix preview mode on Gatsby Cloud.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 const fetchMeta = require(`./lib/fetchMeta`);
 const fetchTheme = require('./lib/fetchTheme');
 
-const { CONTENTFUL_ACCESS_TOKEN, CONTENTFUL_SPACE_ID } = process.env;
+const { CONTENTFUL_ACCESS_TOKEN, CONTENTFUL_PREVIEW_ACCESS_TOKEN, CONTENTFUL_SPACE_ID, CONTENTFUL_HOST } = process.env;
 
 const { siteUrl, title, description, lang, logo } = fetchMeta();
 const { fontFamily, titleFontFamily } = fetchTheme();
@@ -17,8 +17,9 @@ module.exports = {
     {
       resolve: 'gatsby-source-contentful',
       options: {
-        accessToken: CONTENTFUL_ACCESS_TOKEN,
-        spaceId: CONTENTFUL_SPACE_ID
+        accessToken: CONTENTFUL_ACCESS_TOKEN || CONTENTFUL_PREVIEW_ACCESS_TOKEN,
+        spaceId: CONTENTFUL_SPACE_ID,
+        host: CONTENTFUL_HOST
       }
     },
     'gatsby-plugin-postcss',
