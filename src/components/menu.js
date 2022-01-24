624eff2d69e5250fabcaeb132d92155a12ec9a2e
Konsta Purtsi <konsta@identio.fi>
2022-01-24 16:21:06 +0200

    Fix menu overflow.

--- src/components/menu.js
+++ src/components/menu.js
@@ -18,11 +18,18 @@ const Menu = ({ isOpen }) => {
 
   return (
     <nav
-      className={`z-20 fixed top-0 left-0 w-full bg-menu-background flex transform-gpu -translate-y-full transition-all ${
+      className={`z-20 fixed top-0 left-0 w-full bg-menu-background flex justify-center transform-gpu -translate-y-full transition-all ${
         isOpen ? 'transform-none' : ''
       }`}
     >
-      <div className="flex flex-col w-full max-h-screen mx-auto overflow-y-scroll sm:m-10 sm:flex-wrap lg:justify-around max-w-7xl sm:max-h-144 lg:max-h-80">
+      <div
+        className={`
+        flex flex-col w-full max-h-screen max-w-7xl mx-auto overflow-y-auto bg-menu-background
+        sm:m-10 sm:flex-wrap sm:max-h-144
+        lg:justify-around lg:max-h-96
+        xl:flex-row
+      `}
+      >
         {contentfulNavigation.navigationCategories.map(({ name, navigationItems }) => (
           <div key={name} className="mx-6 my-3">
             {contentfulNavigation.navigationCategories.length > 1 && (
