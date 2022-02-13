1fa89e04ac898e1e900866b4b25873f4e6f189f6
Konsta Purtsi <konsta@identio.fi>
2022-02-13 16:48:55 +0200

    Iframe support for content pages.

--- src/templates/page.js
+++ src/templates/page.js
@@ -5,17 +5,36 @@ import Seo from '../components/seo';
 import RichText from '../components/richtext';
 
 const Page = ({ data }) => {
-  const { title, description, lang, body } = data.contentfulPage;
+  const { title, description, lang, body, iframeUrl } = data.contentfulPage;
 
   return (
-    <main className="min-h-[30vh]">
+    <main className="min-h-[30vh] mb-24">
       <Seo title={title} description={description} lang={lang} />
-      <article className="my-3 mb-12 lg:mt-0">
+      <article className="my-3 lg:mt-0">
         <div className="max-w-3xl p-4 mx-auto md:p-8 text-text">
           <RichText data={body} />
         </div>
+        <Iframe url={iframeUrl} />
       </article>
     </main>
+  );
+};
+
+const Iframe = ({ url }) => {
+  if (!url) {
+    return null;
+  }
+
+  return (
+    <div className="relative overflow-hidden mx-auto max-w-3xl h-128">
+      <iframe
+        src={url}
+        className="absolute border-0 w-full h-full top-0 left-0"
+        height="600"
+        frameborder="0"
+        scrolling="no"
+      />
+    </div>
   );
 };
 
@@ -36,6 +55,7 @@ export const query = graphql`
           }
         }
       }
+      iframeUrl
     }
   }
 `;
